a38ef4323ef9a2859498201d9932599faf4cdb8c
Federico Gabriel Gutierrez <gutierrezfedericog@gmail.com>
2023-12-10 22:36:14 -0300

    Se actualiza Menú Responsivo

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,5 +1,7 @@
-// Menú Responsivo
-document.querySelector("button .buttonMenuToggle")
+// Asociar la función de validación al evento de clic en el botón de enviar
+submitButton.addEventListener('click', validarFormulario);
+
+document.querySelector("button.button-menu-toggle")
     .addEventListener("click", function() {
            document.querySelector(".navLinks").
-                      classList.toggle(".navLinksResponsive")})
+                      classList.toggle("navLinksResponsive")})
