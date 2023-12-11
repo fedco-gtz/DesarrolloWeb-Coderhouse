8522c11d9512d3fd8caa3857e9f784f980df9743
Federico Gabriel Gutierrez <gutierrezfedericog@gmail.com>
2023-12-10 22:57:52 -0300

    Actualización de barra de navegación respondive

--- javascripts/scripts.js
+++
@@ -1,7 +0,0 @@
-// Asociar la función de validación al evento de clic en el botón de enviar
-submitButton.addEventListener('click', validarFormulario);
-
-document.querySelector("button.button-menu-toggle")
-    .addEventListener("click", function() {
-           document.querySelector(".navLinks").
-                      classList.toggle("navLinksResponsive")})
